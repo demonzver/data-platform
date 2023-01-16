e982fc9037cb1381e67f32fefd49a3c25efff8ff
itsmy <demonzvermsu@gmail.com>
2023-01-16 04:40:08 +0100

    Edited transform_sql.sql

--- data-platform/sql_requests/transform_sql.sql
+++ data-platform/sql_requests/transform_sql.sql
@@ -2,7 +2,7 @@ INSERT INTO `default`.rate_pairs (`date`, dt, rate1_rate2, base, rate1, rate2)
 SELECT
 	toDateTime(parseDateTimeBestEffortOrNull(replaceRegexpAll(visitParamExtractRaw(json_string, 'date'), '"', ''))) AS `date`,
 	`dt`,
-	'BTC_USD' as rate1_rate2,
+	'BTCUSD' as rate1_rate2,
 	replaceRegexpAll(visitParamExtractRaw(json_string, 'base'), '"', '') as base,
     toFloat64OrNull(trim(BOTH ' ' FROM visitParamExtractRaw(visitParamExtractRaw(json_string, 'rates'), 'BTC'))) as rate1,
     toFloat64OrNull(trim(BOTH ' ' FROM visitParamExtractRaw(visitParamExtractRaw(json_string, 'rates'), 'USD'))) as rate2
