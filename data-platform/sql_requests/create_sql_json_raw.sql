4b56f5a16076763a8508680ad599d9b005c34ed2
itsmy <demonzvermsu@gmail.com>
2023-01-21 22:20:47 +0100

    Formated SQL

--- data-platform/sql_requests/create_sql_json_raw.sql
+++ data-platform/sql_requests/create_sql_json_raw.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS {{ params.json_raw }}
-(dt_add DateTime,
-json_string String)
+(
+    dt_add DateTime,
+    json_string String
+)
 ENGINE = MergeTree
 PARTITION BY toYYYYMM(dt_add)
 ORDER BY dt_add
--- data-platform/sql_requests/create_sql_target.sql
+++ data-platform/sql_requests/create_sql_target.sql
@@ -1,10 +1,12 @@
 CREATE TABLE IF NOT EXISTS {{ params.rate_pairs }}
-(`date` DateTime,
-`dt_add` DateTime,
-`rate1_rate2` Nullable(String),
-`base` Nullable(String),
-`rate1` Nullable(Float64),
-`rate2` Nullable(Float64) )
+(`
+    date` DateTime,
+    `dt_add` DateTime,
+    `rate1_rate2` Nullable(String),
+    `base` Nullable(String),
+    `rate1` Nullable(Float64),
+    `rate2` Nullable(Float64)
+)
 ENGINE = MergeTree
 PARTITION BY toYYYYMM(`date`)
 ORDER BY `date`
