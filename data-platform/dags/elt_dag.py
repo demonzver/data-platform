99f8d77f9d1eb623f9e6b55250ff368e5ac07dec
itsmy <demonzvermsu@gmail.com>
2023-01-29 02:05:36 +0100

    Added SQLTemplatedPythonOperator to plugins

--- data-platform/dags/elt_dag.py
+++ data-platform/dags/elt_dag.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow_clickhouse_plugin.hooks.clickhouse_hook import ClickHouseHook
-from airflow.operators.python_operator import PythonOperator
+# from airflow.operators.python_operator import PythonOperator
+from sql_templated_python_operator import SQLTemplatedPythonOperator
 import pendulum
 import requests
 import json
@@ -10,6 +11,77 @@ json_raw_tbl_name = "default.json_raw"  # Table name for raw json files extracte
 rate_pairs_tbl_name = "default.rate_pairs"  # Table name for result parsed data partitioned by YYYYMM for date column
 api_historical_start_date = "2023-01-01"
 api_symbols = "BTC,USD"
+
+
+def check_table_exists(conn_hook, tablename):
+    query = f"EXISTS TABLE {tablename}"
+    records = conn_hook.get_records(query)
+    return records[0][0]
+
+
+# [START extract_load_function]
+def extract_load(**kwargs):
+    ch_hook = ClickHouseHook(clickhouse_conn_id='clickhouse_default')
+
+    # Request to detection latest date
+    api_request_latest = kwargs["api_request_latest"].format(api_symbols=kwargs["api_symbols"])
+    response_latest = requests.get(api_request_latest)
+
+    if response_latest.status_code == 200:
+        data_latest = response_latest.json()
+    else:
+        print(f"Responce for request {api_request_latest} is bad and status_code: {response_latest.status_code}")
+
+    # Table name to insertion raw json files as json strings
+    json_raw_tbl = kwargs["params"]["json_raw"]
+    print(f"Table to import raw jsons: {json_raw_tbl}")
+
+    if check_table_exists(ch_hook, json_raw_tbl):
+        print(f"Table {json_raw_tbl} to insertion raw json files as json strings already exists")
+
+        # Using same start_date and end_date in api request
+        api_request_historical = kwargs["api_request_historical"].format(start_date=data_latest['date'],
+                                                                         end_date=data_latest['date'],
+                                                                         symbols=kwargs["api_symbols"])
+    else:
+        print(f"Table {json_raw_tbl} to insertion raw json files as json strings not exists")
+
+        # Create table for json data
+        create_sql_json_raw = kwargs["templates_dict"]["query_create_sql_json_raw"]
+        ch_hook.run(create_sql_json_raw)
+
+        # Using start_date from params to get historical data through request
+        api_request_historical = kwargs["api_request_historical"].format(start_date=kwargs["api_historical_start_date"],
+                                                                         end_date=data_latest['date'],
+                                                                         symbols=kwargs["api_symbols"])
+
+    # Main request
+    print(f"api_request: {api_request_historical}")
+    response = requests.get(api_request_historical)
+
+    if response.status_code == 200:
+        data = response.json()
+        data_json_str = json.dumps(data)
+        insert_sql_raw = kwargs["templates_dict"]["query_insert_sql_raw"]
+        insert_sql_raw = insert_sql_raw.format(data_json_str=data_json_str)
+        ch_hook.run(insert_sql_raw)
+    else:
+        print(f"Responce for request {api_request_historical} is bad and status_code: {response.status_code}")
+
+
+# [END extract_load_function]
+
+
+# [START transform_function]
+def transform(**kwargs):
+    ch_hook = ClickHouseHook(clickhouse_conn_id='clickhouse_default')
+    create_sql_target = kwargs["templates_dict"]["query_create_sql_target"]
+    transform_sql = kwargs["templates_dict"]["query_transform_sql"]
+    ch_hook.run(create_sql_target)
+    ch_hook.run(transform_sql)
+
+
+# [END transform_function]
 
 
 with DAG(
@@ -22,84 +94,6 @@ with DAG(
     tags=['zdm'],
     template_searchpath='/opt/airflow/sql_requests',
 ) as dag:
-
-
-    class SQLTemplatedPythonOperator(PythonOperator):
-        template_ext = ('.sql', '.cql')
-
-
-    def check_table_exists(conn_hook, tablename):
-        query = f"EXISTS TABLE {tablename}"
-        records = conn_hook.get_records(query)
-        return records[0][0]
-
-
-    # [START extract_load_function]
-    def extract_load(**kwargs):
-        ch_hook = ClickHouseHook(clickhouse_conn_id='clickhouse_default')
-
-
-        # Request to detection latest date
-        api_request_latest = kwargs["api_request_latest"].format(api_symbols=kwargs["api_symbols"])
-        response_latest = requests.get(api_request_latest)
-
-        if response_latest.status_code == 200:
-            data_latest = response_latest.json()
-        else:
-            print(f"Responce for request {api_request_latest} is bad and status_code: {response_latest.status_code}")
-
-
-        # Table name to insertion raw json files as json strings
-        json_raw_tbl = kwargs["params"]["json_raw"]
-        print(f"Table to import raw jsons: {json_raw_tbl}")
-
-
-        if check_table_exists(ch_hook, json_raw_tbl):
-            print(f"Table {json_raw_tbl} to insertion raw json files as json strings already exists")
-
-            # Using same start_date and end_date in api request
-            api_request_historical = kwargs["api_request_historical"].format(start_date=data_latest['date'],
-                                                                             end_date=data_latest['date'],
-                                                                             symbols=kwargs["api_symbols"])
-        else:
-            print(f"Table {json_raw_tbl} to insertion raw json files as json strings not exists")
-
-            # Create table for json data
-            create_sql_json_raw = kwargs["templates_dict"]["query_create_sql_json_raw"]
-            ch_hook.run(create_sql_json_raw)
-
-            # Using start_date from params to get historical data through request
-            api_request_historical = kwargs["api_request_historical"].format(start_date=kwargs["api_historical_start_date"],
-                                                                             end_date=data_latest['date'],
-                                                                             symbols=kwargs["api_symbols"])
-
-
-        # Main request
-        print(f"api_request: {api_request_historical}")
-        response = requests.get(api_request_historical)
-
-        if response.status_code == 200:
-            data = response.json()
-            data_json_str = json.dumps(data)
-            insert_sql_raw = kwargs["templates_dict"]["query_insert_sql_raw"]
-            insert_sql_raw = insert_sql_raw.format(data_json_str = data_json_str)
-            ch_hook.run(insert_sql_raw)
-        else:
-            print(f"Responce for request {api_request_historical} is bad and status_code: {response.status_code}")
-
-    # [END extract_load_function]
-
-
-    # [START transform_function]
-    def transform(**kwargs):
-        ch_hook = ClickHouseHook(clickhouse_conn_id='clickhouse_default')
-        create_sql_target = kwargs["templates_dict"]["query_create_sql_target"]
-        transform_sql = kwargs["templates_dict"]["query_transform_sql"]
-        ch_hook.run(create_sql_target)
-        ch_hook.run(transform_sql)
-
-    # [END transform_function]
-
 
     # [START main_flow]
     extract_load_task = SQLTemplatedPythonOperator(
